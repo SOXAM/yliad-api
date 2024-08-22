a49d543bf2240c579894e985e0ff8fe92ea77056
Junyeop Kim <junyeopkim@gmail.com>
2024-08-22 22:25:23 +0900

    skeleton from spring initializer

---
+++ src/main/java/com/soxam/yliad/YliadApplication.java
@@ -0,0 +1,13 @@
+package com.soxam.yliad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YliadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YliadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soxam/yliad/YliadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soxam.yliad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YliadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
