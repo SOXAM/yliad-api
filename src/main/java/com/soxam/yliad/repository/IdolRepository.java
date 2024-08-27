db028eff7b28ce8ab3a6a8fb582b3b6d02fb603e
Junyeop Kim <junyeopkim@gmail.com>
2024-08-27 18:29:54 +0900

    Add repository test

---
+++ src/main/java/com/soxam/yliad/repository/IdolRepository.java
@@ -0,0 +1,11 @@
+package com.soxam.yliad.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.soxam.yliad.entity.IdolEntity;
+
+@Repository
+public interface IdolRepository extends JpaRepository<IdolEntity, Long> {
+    
+}
---
+++ src/test/java/com/soxam/yliad/repository/IdolRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.soxam.yliad.repository;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.soxam.yliad.entity.IdolEntity;
+
+@SpringBootTest
+public class IdolRepositoryTest {
+    
+    @Autowired
+    private IdolRepository idolRepository;
+    
+    @Test
+    public void testIdolRepository() {
+        // given
+        IdolEntity idol = new IdolEntity();
+        idol.setName("ISA");
+        idol.setAge(22);
+        idolRepository.save(idol);
+
+        // when
+        IdolEntity isa = idolRepository.findById(idol.getId()).get();
+
+        // then
+        assertEquals(isa.getName(), "ISA");
+        assertEquals(isa.getAge(), Integer.valueOf(22));
+    }
+}
