c226c55c44158207f5b510e14282c47fae927e9b
Junyeop Kim <junyeopkim@gmail.com>
2024-08-30 10:24:23 +0900

    Add health check api

---
+++ src/main/java/com/soxam/yliad/controller/HealthCheckController.java
@@ -0,0 +1,20 @@
+package com.soxam.yliad.controller;
+
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.soxam.yliad.dto.HealthCheckDTO;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/health_check")
+    public HealthCheckDTO healthCheck() {
+        return HealthCheckDTO.builder()
+            .date(LocalDateTime.now())
+            .message("I am alive!!")
+            .build();
+    }
+}
---
+++ src/main/java/com/soxam/yliad/dto/HealthCheckDTO.java
@@ -0,0 +1,19 @@
+package com.soxam.yliad.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class HealthCheckDTO {
+    private LocalDateTime date;
+    private String message;
+}
