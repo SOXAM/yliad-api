2305298f32f81ef65f4027cd7311774377b4ba4b
Junyeop Kim <junyeopkim@gmail.com>
2024-09-03 00:03:35 +0900

    Add mysql datasource configuration

---
+++ src/main/java/com/soxam/yliad/configuration/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package com.soxam.yliad.configuration;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public HikariDataSource mysqlDataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:mysql://localhost/yliad_db")
+                .username("root")
+                .password("isamovingon")
+                .type(HikariDataSource.class)
+                .build();
+    }
+}
