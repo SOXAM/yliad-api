db15a9fafd99436f065102538c76c5e32c310596
Junyeop Kim <junyeopkim@gmail.com>
2024-09-19 22:09:48 +0900

    disable cors

---
+++ src/main/java/com/soxam/yliad/configuration/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.soxam.yliad.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests().anyRequest().permitAll();
+		http.cors(cors -> cors.disable());
+		return http.build();
+	}
+
+}
