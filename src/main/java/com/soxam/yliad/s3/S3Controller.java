253c70ee8d20d5f1884a7f09ec5bdfeb18d997cc
smilejj91 <smilejj91@naver.com>
2024-09-23 22:00:30 +0900

    [feat] s3 bucket에 파일 업로드 기능 추가

--- src/main/java/com/soxam/yliad/s3/S3Controller.java
+++ src/main/java/com/soxam/yliad/s3/S3Controller.java
@@ -33,4 +33,19 @@ public class S3Controller {
 		return "File downloaded to: " + downloadPath;
 	}
 
+	@PostMapping("/upload")
+	public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+		System.out.println("hello");
+		String key = "test/" + file.getOriginalFilename();
+
+		// 파일을 임시 디렉토리에 저장
+		File tempFile = new File(System.getProperty("java.io.tmpdir") + "/" + file.getOriginalFilename());
+		file.transferTo(tempFile);
+
+		// S3에 업로드
+		s3Service.uploadFile(key, tempFile.getAbsolutePath());
+
+		return "File uploaded: " + key;
+	}
+
 }
--- src/main/java/com/soxam/yliad/s3/S3Service.java
+++ src/main/java/com/soxam/yliad/s3/S3Service.java
@@ -1,11 +1,12 @@
 package com.soxam.yliad.s3;
 
+import org.springframework.stereotype.Component;
+
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import org.springframework.stereotype.Component;
-
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 
@@ -45,6 +46,15 @@ public class S3Service {
 			.build();
 
 		s3Client.getObject(getObjectRequest, Paths.get(downloadPath));
+	}
+
+	public void uploadFile(String key, String filePath) {
+		PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+			.bucket(awsProperties.getBucketName())
+			.key(key)
+			.build();
+
+		s3Client.putObject(putObjectRequest, RequestBody.fromFile(Paths.get(filePath)));
 	}
 
 }
